7eb449e8c7ad4d008bb5dd9beedbfa385d81a9a5
JamaCodes <jama.avwork@gmail.com>
2022-01-10 22:19:14 -0600

    js add form collecting/posting data & returning to display

--- CoffeeShop/js-app/apiManager.js
+++ CoffeeShop/js-app/apiManager.js
@@ -4,3 +4,13 @@ const url = "https://localhost:44352/api/beanvariety/";
 export function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
 }
+
+export const addBean = (beanObj) => {
+	return fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(beanObj)
+	})
+}
--- CoffeeShop/js-app/main.js
+++ CoffeeShop/js-app/main.js
@@ -1,5 +1,6 @@
-﻿import {getAllBeanVarieties} from "./apiManager.js"
+﻿import {getAllBeanVarieties, addBean} from "./apiManager.js"
 import {CoffeeList} from "./Coffeelist.Js"
+import {BeanEntry} from "./AddBean.js"
 
     
 
@@ -17,6 +18,36 @@ const showCoffeeList = () => {
 		listElement.innerHTML = CoffeeList(allCoffee);
 	})
 }
-
+const showBeanEntry = () => {
+		const listElement = document.querySelector(".addBean")
+		listElement.innerHTML = BeanEntry();
+}
+showBeanEntry();
 showCoffeeList();
 
+const applicationElement = document.querySelector(".newBean")
+applicationElement.addEventListener("click", event => {
+    if (event.target.id === "newBean__cancel") {
+        //clear the input fields
+    }
+  })
+  
+  applicationElement.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.id === "newBean__submit") {
+    //collect the input values into an object to post to the DB
+      const name = document.querySelector("input[name='name']").value
+      const region = document.querySelector("input[name='region']").value
+   
+      //we have not created a user yet - for now, we will hard code `1`.
+      //we can add the current time as well
+      const beanObject = {
+          name: name,
+          region: region,
+      }
+  
+    // be sure to import from the DataManager
+        addBean(beanObject)
+    }
+    showCoffeeList();
+  })
